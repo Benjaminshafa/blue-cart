d31454d6d1a2bf347a544608131e235d271b542b
Ben <shafabakhsh@campus.tu-berlin.de>
2020-01-12 21:38:47 +0100

    product-service bl

--- product-services/product-info-service/src/server.js
+++ product-services/product-info-service/src/server.js
@@ -2,20 +2,34 @@
 const express = require('express')
 const app = express()
 const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 // for configuring mongoDB connection
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://blue-cart-admin:QwertyMan@product-info-w1twn.mongodb.net/test?retryWrites=true&w=majority";
+const uri = "mongodb+srv://blue-cart-admin:QwertyMan@product-info-w1twn.mongodb.net/product-service?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
+var mongodbConnection = null;
+client.connect(function(err,connection){
     if(err){
         console.log(err);
     }
     else{
+       mongodbConnection = connection.db();
         app.listen(port, () => console.log(`Example app listening on port ${port}!`))
     }
+}); 
+
+var controller = require('./Controller');
+
+app.post('/insert-product',function(req,res){
+    controller.insert_Product(mongodbConnection,req.body,function(mongoItem){
+        if(mongoItem.insertedCount < 1){
+            return res.status(200).send({"message": "failed!"});        
+        }
+        else{
+             return res.status(200).send({});        
+        }
+    })
 });
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
 
