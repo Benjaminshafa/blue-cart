20f84a6d070ad09ee893d06139ccba87418acc78
Ben <shafabakhsh@campus.tu-berlin.de>
2020-02-08 21:14:19 +0100

    update on measurement

--- product-services-gateway/src/server.js
+++ product-services-gateway/src/server.js
@@ -12,7 +12,6 @@ var controller = require('./Controller');
 
 app.get('/product_service_proxy',function(req,res){
     var hrstart = process.hrtime();
-
     controller.doTheMagic(req.headers.product_id,req.headers.customer_id,function(result){
         if(!result){
             return res.status(404).send({"message": "not found!"});        
@@ -22,7 +21,7 @@ app.get('/product_service_proxy',function(req,res){
             var totalAPICallElapsedTime = hrend[0] * 1000 + hrend[1] / 1000000
             console.log('The API call took '+ totalAPICallElapsedTime);
 
-             return res.status(200).send({result,totalRESTCallBetweenServices: result.totalTimeElapsedOfAllServicesCall, GatewayCallDuration: totalAPICallElapsedTime});        
+             return res.status(200).send({result,Total_Inter_Services_Call_On_REST_took: totalAPICallElapsedTime});        
         }
     })
 });
